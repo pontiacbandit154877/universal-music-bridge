d8346f9c737cf2d3a57a81770b0f951f6f83da54
KaironeyBaloney <64377164+KaironeyBaloney@users.noreply.github.com>
2026-04-21 15:59:51 -0400

    50% Conversion of Tidal Test
    50% conversion of Tidal Test script to new data format from Tidal API Key instead of Tidal Account methods

--- tests/tidal_api_test.py
+++ tests/tidal_api_test.py
@@ -3,44 +3,47 @@ from tidal_api import tidal_init
 
 tidal_init()
 
-album_list = tidal_api(category="album", query="I LAY DOWN MY LIFE FOR YOU")
-single_list = tidal_api(category="single", query="Lift Yourself")
-song_list = tidal_api(category="song", query="loop it and leave it")
-artist_list = tidal_api(category="artist", query="JPEGMAFIA")
-compilation_list = tidal_api(category="compilation", query="We Live in a Society")
+album_list, top_album = tidal_api(category="album", query="I LAY DOWN MY LIFE FOR YOU")
+
+single_list, top_single = tidal_api(category="single", query="Lift Yourself")
+## song_list = tidal_api(category="song", query="loop it and leave it")
+## artist_list = tidal_api(category="artist", query="JPEGMAFIA")
+## compilation_list = tidal_api(category="compilation", query="We Live in a Society")
 
 print("Printing Albums...")
 for album in album_list:
-    print(f"ID: {album.id}")
-    print(f"Title: {album.name}")
-    print(f"Duration: {album.duration} seconds")
-    print(f"Artist: {album.artist.name}")
-    print(f"Tracks: {album.num_tracks}\n")
+    attr = album.get('attributes', {})
+    print(f"ID: {album['id']}")
+    print(f"Title: {attr.get('title')}")
+    print(f"Duration: {attr.get('duration')}")
+    print(f"Tracks: {attr.get('numberOfItems')}\n")
+
+print("Top Album:")
+attr = top_album.get('attributes', {})
+print(f"ID: {top_album['id']}")
+print(f"Title: {attr.get('title')}")
 
 print("Printing Singles...")
 for single in single_list:
-    print(f"ID: {single.id}")
-    print(f"Title: {single.name}")
-    print(f"Duration: {single.duration} seconds")
-    print(f"Artist: {single.artist.name}")
-    print(f"Tracks: {single.num_tracks}\n")
+    attr = single.get('attributes', {})
+    print(f"ID: {single['id']}")
+    print(f"Title: {attr.get('title')}")
+    print(f"Duration: {attr.get('duration')}")
+    print(f"Tracks: {attr.get('numberOfItems')}\n")
+
+print("Top Single:")
+attr = top_single.get('attributes', {})
+print(f"ID: {top_single['id']}")
+print(f"Title: {attr.get('title')}")
 
 print("Printing Songs...")
 for song in song_list:
-    print(f"ID: {song.id}")
-    print(f"Title: {song.name}")
-    print(f"Duration: {song.duration} seconds")
-    print(f"Artist: {song.artist.name}\n")
+    attr = song.get('attributes', {})
+    print(f"ID: {song['id']}")
+    print(f"Title: {attr.get('title')}\n")
 
 print("Printing Artists...")
 for artist in artist_list:
-    print(f"ID: {artist.id}")
-    print(f"Name: {artist.name}\n")
-
-print("Printing Compilations...")
-for compilation in compilation_list:
-    print(f"ID: {compilation.id}")
-    print(f"Title: {compilation.name}")
-    print(f"Duration: {compilation.duration} seconds")
-    print(f"Artist: {compilation.artist.name}")
-    print(f"Tracks: {compilation.num_tracks}\n")
+    attr = artist.get('attributes', {})
+    print(f"ID: {artist['id']}")
+    print(f"Name: {attr.get('name')}\n")
