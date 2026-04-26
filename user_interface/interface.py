dfddd7aae98260682ecdd1579ef8d6df22ecffe8
malakc-umd <malakc@umich.edu>
2026-04-26 09:16:10 -0400

    added style dictionaries and modified code

--- user_interface/interface.py
+++ user_interface/interface.py
@@ -2,18 +2,100 @@ import tkinter as tk
 import webbrowser
 from main import search_apis
 
+# class to change button color when you hover
+class HoverButton(tk.Button):
+    def __init__(self, master=None, hover_bg=None, **kwargs):
+        self.default_bg = kwargs.get('bg', 'SystemButtonFace')
+        self.hover_bg = hover_bg if hover_bg else self.default_bg
+
+        super().__init__(master, **kwargs)
+
+        self.bind("<Enter>", self.on_hover)
+        self.bind("<Leave>", self.on_leave)
+
+    def on_hover(self, event):
+        self.config(bg=self.hover_bg)
+
+    def on_leave(self, event):
+        self.config(bg=self.default_bg)
+
+
 # initialize main application window
 window = tk.Tk()
 window.title("Universal Music Bridge")
 window.geometry("900x800")
-window.configure(bg="#dff6ff")  # light blue
+
+# defining themes
+
+# main app background
+APP_BG = "#dff6ff"
+APP_FG = "#0077b6"
+window.configure(bg=APP_BG)
+
+# style for title
+TITLE_STYLE = {
+    "bg": APP_BG,
+    "fg": APP_FG,
+    "font": ("Segoe UI", 25)
+}
+
+# style for standard labels
+LABEL_STYLE = {
+    "bg": APP_BG,
+    "fg": APP_FG,
+    "font": ("Segoe UI", 20)
+}
+#style for small labels
+SMALL_LABEL_STYLE = {
+    "bg": APP_BG,
+    "fg": APP_FG,
+    "font": ("Segoe UI", 16)
+}
+
+# style for buttons
+BUTTON_STYLE = {
+    "bg": "#90e0ef",
+    "hover_bg": "#ade8f4",
+    "fg": "black",
+    "font": ("Segoe UI", 12, "bold"),
+    "relief": "flat",
+    "padx": 10,
+    "pady": 5
+}
+
+# style for result buttons
+RESULT_BUTTON_STYLE = {
+    "bg": "#caf0f8",
+    "hover_bg": "#e0fbfc",
+    "fg": "#0077b6",
+    "font": ("Segoe UI", 12),
+    "width": 45,
+    "relief": "groove"
+}
+# style for check box
+CHECKBOX_STYLE = {
+    "font": ("Segoe UI", 12),
+    "bg": APP_BG,
+    "activebackground": APP_BG
+}
+
+RESULTS_LABEL_STYLE = {
+    "font": ("Segoe UI", 12),
+    "fg": "gray",
+    "bg": APP_BG
+}
+
+RESULT_SECTION_STYLE = {
+    "bg": APP_BG,
+    "fg": APP_FG,
+    "font": ("Segoe UI", 12, "bold")
+}
 
 # app title label
 title_label = tk.Label(
     window,
     text="Universal Music Bridge",
-    font=("Segoe UI", 25),
-    bg="#dff6ff", fg="#0077b6"
+    **TITLE_STYLE
 )
 title_label.pack(pady=15)
 
@@ -21,8 +103,7 @@ title_label.pack(pady=15)
 song_label = tk.Label(
     window,
     text="Song Name",
-    font=("Segoe UI", 20),
-    bg="#dff6ff", fg="#0077b6"
+    **LABEL_STYLE,
 )
 song_label.pack(pady=5)
 
@@ -52,8 +133,7 @@ song_entry.bind("<FocusOut>", add_placeholder)
 artist_label = tk.Label(
     window,
     text="Artist Name",
-    font=("Segoe UI", 20),
-    bg="#dff6ff", fg="#0077b6"
+    **LABEL_STYLE
 )
 artist_label.pack(pady=5)
 
@@ -80,12 +160,11 @@ artist_entry.bind("<FocusOut>", add_artist_placeholder)
 filter_label = tk.Label(
     window,
     text="Choose platforms: ",
-    font=("Segoe UI", 16),
-    bg="#dff6ff", fg="#0077b6"
+    **SMALL_LABEL_STYLE
 )
 filter_label.pack(pady=10)
 
-checkbox_frame = tk.Frame(window, bg="#dff6ff")
+checkbox_frame = tk.Frame(window, bg=APP_BG)
 checkbox_frame.pack(pady=5)
 
 youtube_var = tk.BooleanVar()
@@ -94,35 +173,37 @@ tidal_var = tk.BooleanVar()
 
 youtube_checkbox = tk.Checkbutton(
     checkbox_frame,
-    text="YouTube Music",
+    text="Youtube Music",
     variable=youtube_var,
-    font=("Segoe UI", 12),
-    bg="#dff6ff",
-    activebackground="#dff6ff"
+    **CHECKBOX_STYLE
 )
 youtube_checkbox.pack(side="left", padx=15)
 
 spotify_checkbox = tk.Checkbutton(
     checkbox_frame,
-    text="Spotify ",
+    text="Spotify",
     variable=spotify_var,
-    font=("Segoe UI", 12),
-    bg="#dff6ff",
-    activebackground="#dff6ff"
+    **CHECKBOX_STYLE
 )
 spotify_checkbox.pack(side="left", padx=15)
 
 tidal_checkbox = tk.Checkbutton(
     checkbox_frame,
-    text="Tidal ",
+    text="Tidal",
     variable=tidal_var,
-    font=("Segoe UI", 12),
-    bg="#dff6ff",
-    activebackground="#dff6ff"
+    **CHECKBOX_STYLE
 )
 tidal_checkbox.pack(side="left", padx=15)
 
 # search function
+
+def create_result_button(parent, text, link):
+    HoverButton(
+        parent,
+        text=text,
+        command=lambda: webbrowser.open(link),
+        **RESULT_BUTTON_STYLE
+    ).pack(pady=5)
 
 def search():
     for widget in results_frame.winfo_children():
@@ -130,6 +211,17 @@ def search():
 
     song = song_entry.get()
     artist = artist_entry.get()
+
+    if song == "Type a song...":
+        song = ""
+    if artist == "Type an artist...":
+        artist = ""
+
+    query = f"{song} {artist}".strip()
+
+    if not query:
+        results_label.config(text="Please enter a song or artist.")
+        return
 
     apis = []
 
@@ -152,59 +244,45 @@ def search():
         )
 
         if youtube_var.get() and youtube_results:
-            tk.Label(results_frame, text="YouTube Results:", bg="#dff6ff").pack()
+            tk.Label(results_frame, text="YouTube Results:", **RESULT_SECTION_STYLE).pack()
 
             for result in youtube_results[0][:4]:
-                    tk.Button(
-                        results_frame,
-                        text=result["title"] + " - " + result["artist"],
-                        font=("Segoe UI", 12),
-                        width=45,
-                        bg="#caf0f8",
-                        fg="#0077b6",
-                        relief="groove",
-                        command=lambda link=result["link"]: webbrowser.open(link)
-                    ).pack(pady=5)
+                create_result_button(
+                    results_frame,
+                    result["title"] + " - " + result["artist"],
+                    result["link"]
+                )
 
         if spotify_var.get() and spotify_results:
-            tk.Label(results_frame, text="Spotify Results:", bg="#dff6ff", fg="#0077b6").pack()
+            tk.Label(results_frame, text="Spotify Results:", **RESULT_SECTION_STYLE).pack()
 
             for result in spotify_results[0][:4]:
-                tk.Button(
+                create_result_button(
                     results_frame,
-                    text=result["title"] + " - " + result["artist"],
-                    width=45,
-                    bg="#caf0f8",
-                    fg="#0077b6",
-                    relief="groove",
-                    command=lambda link=result["link"]: webbrowser.open(link)
-                ).pack(pady=5)
+                    result["title"] + " - " + result["artist"],
+                    result["link"]
+                )
 
         if tidal_var.get() and tidal_results and isinstance(tidal_results[0], dict):
-            tk.Label(results_frame, text="Tidal Result:", bg="#dff6ff").pack()
+            tk.Label(results_frame, text="Tidal Results:", **RESULT_SECTION_STYLE).pack()
 
             result = tidal_results[0] if isinstance(tidal_results, list) else tidal_results
 
-            tk.Button(
+            create_result_button(
                 results_frame,
-                text=result["title"] + " - " + result.get("artist", "Unknown"),
-                command=lambda link=result["link"]: webbrowser.open(link)
-            ).pack(pady=5)
+                result["title"] + " - " + result.get("artist", "Unknown"),
+                result["link"]
+            )
 
     else:
         results_label.config(text="No platform selected")
 
 
-search_button = tk.Button(
+search_button = HoverButton(
     window,
     text="Search",
-    font=("Segoe UI", 12, "bold"),
-    bg="#90e0ef",
-    fg="black",
-    relief="flat",
-    padx=10,
-    pady=5,
-    command=search
+    command=search,
+    **BUTTON_STYLE
 )
 search_button.pack(pady=20)
 
@@ -218,7 +296,7 @@ results_label = tk.Label(
 results_label.pack(pady=10)
 
 # result container
-results_frame = tk.Frame(window, bg="#dff6ff")
+results_frame = tk.Frame(window, bg=APP_BG)
 results_frame.pack(pady=20)
 
 
